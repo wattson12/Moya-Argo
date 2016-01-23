4598af33b8998810cb86a5000739e7c24f151cb4
Sam Watts <samuel.watts@gmail.com>
2016-01-23 12:12:48 +0000

    first pod install

---
+++ Example/Pods/Target Support Files/Moya-Argo/Moya-Argo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Moya_Argo : NSObject
+@end
+@implementation PodsDummy_Moya_Argo
+@end
---
+++ Example/Pods/Target Support Files/Moya-Argo/Moya-Argo-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Moya_ArgoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Moya_ArgoVersionString[];
+
