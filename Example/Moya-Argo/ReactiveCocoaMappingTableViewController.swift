772a7e6f21568479eb015b8c59ff05d68492b0f3
Sam Watts <samuel.watts@gmail.com>
2016-01-23 19:28:07 +0000

    map users and user in reactive cocoa demo

--- Example/Moya-Argo/ReactiveCocoaMappingTableViewController.swift
+++ Example/Moya-Argo/ReactiveCocoaMappingTableViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import Moya
+import ReactiveCocoa
+import Moya_Argo
 
 class ReactiveCocoaMappingTableViewController: DemoBaseTableViewController {
+
+    let provider:ReactiveCocoaMoyaProvider<DemoTarget> = ReactiveCocoaMoyaProvider(stubClosure: { _ in return .Immediate })
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,4 +21,35 @@ class ReactiveCocoaMappingTableViewController: DemoBaseTableViewController {
         self.title = "ReactiveCocoa"
     }
 
+    //MARK: Overrides
+    override func fetchUsers() {
+        
+        provider.request(.AllUsers).mapArray(ArgoUser.self, rootKey: "users").observeOn(UIScheduler()).start { event in
+
+            switch event {
+            case .Next(let users):
+                self.users = users.map { $0 }
+                self.tableView.reloadData()
+            case .Failed(let error):
+                print("error: \(error)")
+            default: break
+            }
+        }
+    }
+    
+    override func fetchUserDetail(user: UserType, showAlertClosure: (UserType) -> ()) {
+        
+        provider.request(.User(userID: user.id.description)).mapObject(ArgoUser).observeOn(UIScheduler()).start { event in
+            
+            switch event {
+            case .Next(let user):
+                showAlertClosure(user)
+            case .Failed(let error):
+                print("error: \(error)")
+            default:
+                break
+            }
+        }
+        
+    }
 }
