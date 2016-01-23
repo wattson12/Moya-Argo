f3dd737d7ea3fffbec835418f64e7a2538add5d2
Sam Watts <samuel.watts@gmail.com>
2016-01-23 13:24:31 +0000

    Added argo mapping view controller

---
+++ Example/Moya-Argo/MoyaMappingTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MoyaMappingTableViewController.swift
+//  Moya-Argo
+//
+//  Created by Sam Watts on 23/01/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class MoyaMappingTableViewController: DemoBaseTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Moya Mapping"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
