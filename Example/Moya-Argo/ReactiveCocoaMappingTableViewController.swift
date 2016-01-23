25e71b051f3cb5452dc6fd324cb55ec078318747
Sam Watts <samuel.watts@gmail.com>
2016-01-23 19:15:42 +0000

    Added reactive cocoa demo view controller

---
+++ Example/Moya-Argo/ReactiveCocoaMappingTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReactiveCocoaMappingTableViewController.swift
+//  Moya-Argo
+//
+//  Created by Sam Watts on 23/01/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class ReactiveCocoaMappingTableViewController: DemoBaseTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "ReactiveCocoa"
+    }
+
+}
