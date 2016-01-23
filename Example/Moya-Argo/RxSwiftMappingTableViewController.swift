dfa3a225baf7bd36150b4ea9f5566159acd70031
Sam Watts <samuel.watts@gmail.com>
2016-01-23 18:30:40 +0000

    added RxSwift demo view controller

---
+++ Example/Moya-Argo/RxSwiftMappingTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RxSwiftMappingTableViewController.swift
+//  Moya-Argo
+//
+//  Created by Sam Watts on 23/01/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class RxSwiftMappingTableViewController: DemoBaseTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "RxSwift"
+    }
+
+}
