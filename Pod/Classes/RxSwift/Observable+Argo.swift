4f16156da2f4c971f0203f60938ca2b988b33845
Sam Watts <samuel.watts@gmail.com>
2016-01-28 20:45:35 +0000

    reworked mapArray and mapObject to follow example from RxSwift docs

--- Pod/Classes/RxSwift/Observable+Argo.swift
+++ Pod/Classes/RxSwift/Observable+Argo.swift
@@ -15,85 +15,56 @@ public extension ObservableType where E == Moya.Response {
     
     public func mapObject<T: Decodable where T == T.DecodedType>(type: T.Type, rootKey: String? = nil) -> Observable<T> {
         
-        return flatMap { response in
+        return Observable.create { observer in
             
-            return Observable.create { observer in
+            self.subscribe { event in
                 
-                do {
-                    let JSON = try NSJSONSerialization.JSONObjectWithData(response.data, options: [])
-                    
-                    let decoded:Decoded<T>
-                    if let rootKey = rootKey {
-                        decoded = decodeWithRootKey(rootKey, JSON)
-                    } else {
-                        decoded = decode(JSON)
+                switch event {
+                case .Next(let response):
+                    do {
+                        let mappedObject:T = try response.mapObject(rootKey)
+                        observer.onNext(mappedObject)
+                    } catch {
+                        observer.onError(error)
                     }
-                    
-                    observer.onForDecoded(decoded)
-                    
-                    observer.on(.Completed)
-                    
-                } catch {
-                    
-                    observer.on(.Error(error))
+                case .Error(let error):
+                    observer.onError(error)
+                case .Completed:
+                    observer.onCompleted()
                 }
-                
-                return AnonymousDisposable { }
             }
         }
     }
     
     public func mapObjectWithRootKey<T: Decodable where T == T.DecodedType>(type: T.Type, rootKey: String) -> Observable<T> {
-     
         return mapObject(type, rootKey: rootKey)
     }
     
     public func mapArray<T: Decodable where T == T.DecodedType>(type: T.Type, rootKey: String? = nil) -> Observable<[T]> {
         
-        return flatMap { response -> Observable<[T]> in
+        return Observable.create { observer in
             
-            return Observable.create { observer in
+            self.subscribe { event in
                 
-                do {
-                    let JSON = try NSJSONSerialization.JSONObjectWithData(response.data, options: [])
-                    
-                    let decoded:Decoded<[T]>
-                    if let rootKey = rootKey {
-                        decoded = decodeWithRootKey(rootKey, JSON)
-                    } else {
-                        decoded = decode(JSON)
+                switch event {
+                case .Next(let response):
+                    do {
+                        let mappedObject:[T] = try response.mapArray(rootKey)
+                        observer.onNext(mappedObject)
+                    } catch {
+                        observer.onError(error)
                     }
-                    
-                    observer.onForDecoded(decoded)
-                    
-                    observer.on(.Completed)
-                } catch {
-                    
-                    observer.on(.Error(error))
+                case .Error(let error):
+                    observer.onError(error)
+                case .Completed:
+                    observer.onCompleted()
                 }
-                
-                return AnonymousDisposable { }
             }
         }
     }
     
     public func mapArrayWithRootKey<T: Decodable where T == T.DecodedType>(type: T.Type, rootKey: String) -> Observable<[T]> {
-        
         return mapArray(type, rootKey: rootKey)
-    }
-
-}
-
-extension AnyObserver {
-    
-    private func onForDecoded(decoded: Decoded<Element>) {
-        
-        switch decoded {
-        case .Success(let value):
-            self.on(.Next(value))
-        case .Failure(let error):
-            self.on(.Error(error))
-        }
     }
 
 }
